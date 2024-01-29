7584979ddeb66bfeecb05e67520292987a092cef
bregginr <bregginr@rose-hulman.edu>
2024-01-29 13:05:40 -0500

    Game Object, made test coin appear on frame - code from class time

---
+++ src/mainApp/GameObject.java
@@ -0,0 +1,32 @@
+package mainApp;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+/**
+ * TODO: ADD JAVA DOC HERE
+ */
+public class GameObject {
+	
+	private int x, y;
+	private Color color;
+	private String type;
+
+	public GameObject(int x, int y, Color color, String type) {
+		// TODO Auto-generated constructor stub
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.type = type;
+		
+	}
+	
+	public void drawOn(Graphics2D g) {
+		g = (Graphics2D)g.create();
+		g.setColor(this.color);
+		g.fillRect(x, y, 20, 20);
+	}
+	
+	
+
+}
--- src/mainApp/MainComponent.java
+++ src/mainApp/MainComponent.java
@@ -1,5 +1,9 @@
 package mainApp;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JComponent;
 
 /**
@@ -7,11 +11,25 @@ import javax.swing.JComponent;
  */
 public class MainComponent extends JComponent{
 	
-	
+	GameObject testCoin = new GameObject(500,500,Color.ORANGE, "TEST");
 
 	public MainComponent() {
 		// TODO Auto-generated constructor stub
 		System.out.println("In MainComponent File");
+		
+		
+		
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D)g;
+		
+		testCoin.drawOn(g2);
+		
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+		
 	}
 
 }
