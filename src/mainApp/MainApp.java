2f5b626a633d5bdf8a834183f7e7beae1b2ba12f
bregginr <bregginr@rose-hulman.edu>
2024-01-29 11:28:33 -0500

    Added timer, connected MainComponent to MainApp

--- src/mainApp/MainApp.java
+++ src/mainApp/MainApp.java
@@ -1,13 +1,17 @@
 package mainApp;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
- * Class: MainApp
+ * Class: MainApp AKA MainViewer
  * @author w24_a203
  * <br>Purpose: Top level class for CSSE220 Project containing main method 
  * <br>Restrictions: None
@@ -15,21 +19,36 @@ import javax.swing.JPanel;
 public class MainApp {
 	
 	public static final Dimension FRAME_SIZE = new Dimension(1500, 800);
+	public static final int DELAY = 50;
 	
 	private void runApp() {
 		System.out.println("Write your cool arcade game here!");	
 		
-		JFrame myFrame = new JFrame();
-		myFrame.setSize(FRAME_SIZE);
-		myFrame.setTitle("JETPACK JOYRIDE!");
+		JFrame frame = new JFrame();
+		frame.setSize(FRAME_SIZE);
+		frame.setTitle("JETPACK JOYRIDE!");
+		
+		MainComponent mainComponent = new MainComponent();
+		frame.add(mainComponent);
 
+		Timer timer = new Timer(DELAY, new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+//				mainComponent.update(null);
+				mainComponent.repaint();
+				
+				frame.repaint();
+			}
+		});
 		
+		timer.start();
 		
-		
-		
-		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		myFrame.pack();
-		myFrame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.pack();
+		frame.setVisible(true);
 		
 	} // runApp
 
---
+++ src/mainApp/MainComponent.java
@@ -0,0 +1,17 @@
+package mainApp;
+
+import javax.swing.JComponent;
+
+/**
+ * TODO: ADD JAVA DOC HERE
+ */
+public class MainComponent extends JComponent{
+	
+	
+
+	public MainComponent() {
+		// TODO Auto-generated constructor stub
+		System.out.println("In MainComponent File");
+	}
+
+}
