5807c023bd8a8eb9e17a4f8b40de0280542a2310
carterxp <carterxp@rose-hulman.edu>
2024-02-05 11:16:13 -0500

    Gravity more fluid

--- src/mainApp/Hero.java
+++ src/mainApp/Hero.java
@@ -10,32 +10,47 @@ import java.awt.event.KeyListener;
  */
 public class Hero extends GameObject {
 	private int velocity;
+	private boolean fly;
 	
 	public Hero(int x, int y, Color color, Character type) {
 		super(x, y, color, type);
 		this.velocity = 0;
+		this.fly = false;
 	}
 	public void move() {
-		if(super.getY() > MainApp.SCREEN_HEIGHT -80 && velocity > 0) {
+		if(fly) {
+			velocity += -1;
+		}
+		else {
+			velocity += 1;
+		}
+		if(super.getY() > MainApp.SCREEN_HEIGHT - 70 && velocity > 0) {
 			velocity = 0;
+			super.setY(MainApp.SCREEN_HEIGHT - 70);
+		}
+		if(super.getY() < 0 && velocity < 0) {
+			velocity = 0;
+			super.setY(0);
 		}
 		super.setY(getY() + velocity);
 	}
 
 	public void fly() {
-		System.out.println(super.getY());
-		if (super.getY() > 0) {
-			velocity = -10;
-		} else {
-			velocity = 0;
-		}
+//		System.out.println(super.getY());
+//		if (super.getY() > 0) {
+//			velocity = -10;
+//		} else {
+//			velocity = 0;
+//		}
+		this.fly = true;
 	}
 
 	public void stopFlying() {
-		if (super.getY() > MainApp.SCREEN_HEIGHT -80) {
-			velocity = 0;
-		}else {
-			velocity = 10;
-		}
+//		if (super.getY() > MainApp.SCREEN_HEIGHT -80) {
+//			velocity = 0;
+//		}else {
+//			velocity = 10;
+//		}
+		this.fly = false;
 	}
 }
